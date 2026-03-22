09cd572ddd6f64a64529597cb15bbc629c2ad227
AaronE0811 <aaronelizondo945@gmail.com>
2026-03-21 23:23:11 -0600

    url fetch

--- app/services/form.ts
+++ app/services/form.ts
@@ -5,7 +5,7 @@ interface FormData {
   Insulin: number;
   BMI: number;
 }
-const api_url = "/api/predict";
+const api_url = "https://predict-diabetes-phi.vercel.app/predict";
 
 export const predictDiabetes = async ({
   datosForm,
